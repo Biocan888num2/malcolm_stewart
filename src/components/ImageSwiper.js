9f27f216e2b431ddbe81d1c8b1e157c077fb23c4
Andrew Warnke <bepecinplus@gmail.com>
2024-12-23 10:34:22 -0500

    Click Swiper slide pops up Modal

--- src/components/ImageSwiper.js
+++ src/components/ImageSwiper.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'; 
 import 'swiper/css'; 
 import 'swiper/css/navigation'; 
@@ -9,30 +10,56 @@ import '../styles/ImageSwiper.css'; // Import the CSS file
 
 const ImageSwiper = ({ images }) => {
 
+  const [modalImage, setModalImage] = useState(null); 
+  
   const handleImageClick = (src) => { 
-    window.open(src, '_blank'); 
+    setModalImage(src); 
+  }; 
+  
+  const closeModal = () => { 
+    setModalImage(null); 
   };
 
+  const handleModalClick = (e) => { 
+    if (e.target === e.currentTarget) { 
+      closeModal(); 
+    } 
+  };
+
+  // const handleImageClick = (src) => { 
+  //   window.open(src, '_blank'); 
+  // };
+
   return (
-    <Swiper
-        modules={[Navigation, Pagination]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        className="swiper" // Apply the swiper class
-    >
-      {images.map((image, index) => (
-        <SwiperSlide key={index} className="swiper-slide">
-          <img 
-            src={image} 
-            alt={`Slide ${index}`} 
-            onClick={() => handleImageClick(image)} 
-            className="swiper-image"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          className="swiper" // Apply the swiper class
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index} className="swiper-slide">
+            <img 
+              src={image} 
+              alt={`Slide ${index}`} 
+              onClick={() => handleImageClick(image)} 
+              className="swiper-image"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {modalImage && ( 
+        <div className="modal visible" onClick={handleModalClick}> 
+          <div className="modal-content"> 
+            <img src={modalImage} alt="Full size view" /> 
+            <button className="close-button" onClick={closeModal}>×</button> 
+          </div> 
+        </div>
+      )}
+    </>
   );
 };
 
