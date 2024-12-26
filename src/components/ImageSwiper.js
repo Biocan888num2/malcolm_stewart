6c117385eae55a8a418b1d79f7a6865c6befd343
Andrew Warnke <bepecinplus@gmail.com>
2024-12-26 14:18:04 -0500

    Remove double-click zoom. Restart zoom-in from scratch

--- src/components/ImageSwiper.js
+++ src/components/ImageSwiper.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+// import { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'; 
 import 'swiper/css'; 
 import 'swiper/css/navigation'; 
@@ -10,69 +10,30 @@ import '../styles/ImageSwiper.css'; // Import the CSS file
 
 const ImageSwiper = ({ images }) => {
 
-  const [isZoomed, setIsZoomed] = useState(false); 
-  
-  useEffect(() => { 
-    const handleDoubleClick = (e) => { 
-      e.preventDefault(); 
-      
-      if (!isZoomed) { 
-        document.body.style.transform = 'scale(1.5)'; // Adjust zoom scale as necessary 
-        document.body.style.transformOrigin = 'center center'; 
-        
-        // document.body.style.overflow = 'hidden';
-        // Prevent scrolling while zoomed 
-        
-      } else { 
-        document.body.style.transform = 'scale(1)'; 
-        
-        // document.body.style.overflow = 'auto'; 
-        // Restore scrolling 
-      }  
-      setIsZoomed(!isZoomed); 
-    }; 
-    
-    document.addEventListener('dblclick', handleDoubleClick); 
-    
-    return () => { 
-      document.removeEventListener('dblclick', handleDoubleClick); 
-    
-    }; 
-  }, [isZoomed]);
-
-  const handleTouchEnd = (event) => { 
-    if (event.detail === 2) { 
-      handleDoubleClick(event); 
-    } 
-  };
-
   const handleImageClick = (src) => { 
     window.open(src, '_blank'); 
   };
 
-  return (  
-    <div onTouchEnd={handleTouchEnd} ref={handleTouchEnd}>
-      <Swiper
-          modules={[Navigation, Pagination]}
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          className="swiper" // Apply the swiper class
-          // touchEventsTarget="wrapper" // Ensure touch events are handled
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index} className="swiper-slide">
-            <img 
-              src={image} 
-              alt={`Slide ${index}`} 
-              onClick={() => handleImageClick(image)} 
-              className="swiper-image"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>  
+  return (
+    <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        className="swiper" // Apply the swiper class
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index} className="swiper-slide">
+          <img 
+            src={image} 
+            alt={`Slide ${index}`} 
+            onClick={() => handleImageClick(image)} 
+            className="swiper-image"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
