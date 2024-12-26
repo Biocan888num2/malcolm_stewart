8ed9b556147cfd651489ade435efdcd13807d448
Andrew Warnke <bepecinplus@gmail.com>
2024-12-26 12:40:00 -0500

    Swiper: replaced Modal with Zoom-In

--- src/components/ImageSwiper.js
+++ src/components/ImageSwiper.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react'
+import { useState, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react'; 
 import 'swiper/css'; 
 import 'swiper/css/navigation'; 
@@ -9,57 +9,48 @@ import { Navigation, Pagination } from 'swiper/modules';
 import '../styles/ImageSwiper.css'; // Import the CSS file 
 
 const ImageSwiper = ({ images }) => {
+  const [isZoomed, setIsZoomed] = useState(false);
+  const zoomRef = useRef(null);
 
-  const [modalImage, setModalImage] = useState(null); 
-  
-  const handleImageClick = (src) => { 
-    setModalImage(src); 
-  }; 
-  
-  const closeModal = () => { 
-    setModalImage(null); 
+  const handleDoubleClick = () => {
+    if (isZoomed) {
+      zoomRef.current.style.transform = 'scale(1)';
+    } else {
+      zoomRef.current.style.transform = 'scale(2)'; // Adjust zoom scale as necessary
+    }
+    setIsZoomed(!isZoomed);
   };
-
-  const handleModalClick = (e) => { 
-    if (e.target === e.currentTarget) { 
-      closeModal(); 
-    } 
-  };
-
-  // const handleImageClick = (src) => { 
-  //   window.open(src, '_blank'); 
-  // };
 
   return (
-    <>
-      <Swiper
-          modules={[Navigation, Pagination]}
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          className="swiper" // Apply the swiper class
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index} className="swiper-slide">
+    <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        className="swiper" // Apply the swiper class
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index} className="swiper-slide">
+          <div
+            ref={zoomRef}
+            className={`swiper-zoom-container ${isZoomed ? 'zoomed' : ''}`}
+            onDoubleClick={handleDoubleClick}
+            onTouchEnd={(event) => {
+              if (event.detail === 2) {
+                handleDoubleClick();
+              }
+            }}
+          >
             <img 
               src={image} 
               alt={`Slide ${index}`} 
-              onClick={() => handleImageClick(image)} 
               className="swiper-image"
             />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {modalImage && ( 
-        <div className="modal visible" onClick={handleModalClick}> 
-          <div className="modal-content"> 
-            <img src={modalImage} alt="Full size view" /> 
-            <button className="close-button" onClick={closeModal}>×</button> 
-          </div> 
-        </div>
-      )}
-    </>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
