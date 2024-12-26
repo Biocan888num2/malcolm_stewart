9b11bf899631668a978574719805f20a5d21559c
Andrew Warnke <bepecinplus@gmail.com>
2024-12-26 13:01:22 -0500

    Swiper back to step 1

--- src/components/ImageSwiper.js
+++ src/components/ImageSwiper.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'; 
 import 'swiper/css'; 
 import 'swiper/css/navigation'; 
@@ -10,56 +10,30 @@ import '../styles/ImageSwiper.css'; // Import the CSS file
 
 const ImageSwiper = ({ images }) => {
 
-  const [modalImage, setModalImage] = useState(null); 
-  
   const handleImageClick = (src) => { 
-    setModalImage(src); 
-  }; 
-  
-  const closeModal = () => { 
-    setModalImage(null); 
+    window.open(src, '_blank'); 
   };
-
-  const handleModalClick = (e) => { 
-    if (e.target === e.currentTarget) { 
-      closeModal(); 
-    } 
-  };
-
-  // const handleImageClick = (src) => { 
-  //   window.open(src, '_blank'); 
-  // };
 
   return (
-    <>
-      <Swiper
-          modules={[Navigation, Pagination]}
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          className="swiper" // Apply the swiper class
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index} className="swiper-slide">
-            <img 
-              src={image} 
-              alt={`Slide ${index}`} 
-              onClick={() => handleImageClick(image)} 
-              className="swiper-image"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {modalImage && ( 
-        <div className="modal visible" onClick={handleModalClick}> 
-          <div className="modal-content"> 
-            <img src={modalImage} alt="Full size view" /> 
-            <button className="close-button" onClick={closeModal}>×</button> 
-          </div> 
-        </div>
-      )}
-    </>
+    <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        className="swiper" // Apply the swiper class
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index} className="swiper-slide">
+          <img 
+            src={image} 
+            alt={`Slide ${index}`} 
+            onClick={() => handleImageClick(image)} 
+            className="swiper-image"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
